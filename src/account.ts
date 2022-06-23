b7dcb0321c9070da713a78db4e037d90778a1cb6
tangliang <tlyspa@gmail.com>
2022-06-23 17:22:59 +0800

    feat: account secrets transfrom complete

--- src/account.ts
+++ src/account.ts
@@ -4,3 +4,7 @@ import bs58 from 'bs58';
 export const getBs58PrivateKey = (keypair: Keypair) => {
   return bs58.encode(keypair.secretKey);
 };
+
+export const getKeypairFromBs58PrivateKey = (privatekey: string) => {
+  return Keypair.fromSecretKey(bs58.decode(privatekey));
+};
--- test/account.test.ts
+++ test/account.test.ts
@@ -1,27 +1,19 @@
 import * as Web3 from '@solana/web3.js';
-import bs58 from 'bs58';
 
-import KeypairID_TANG from '../.tmp/data/keypaires/TANGL.json';
-import KeypairID_ZCLUB from '../.tmp/data/keypaires/ZCLUB.json';
 import { Account } from '../src';
 
-describe('Account TEST', () => {
-  let connection: Web3.Connection;
-  let keypair1 = Web3.Keypair.fromSecretKey(Uint8Array.from(KeypairID_TANG));
-  let keypair2 = Web3.Keypair.fromSecretKey(Uint8Array.from(KeypairID_ZCLUB));
+describe('Account Secrets Transform', () => {
+  let keypair1 = Web3.Keypair.generate();
 
-  test('', () => {
-    console.log(keypair1.publicKey.toBase58());
-    console.log(Account.getBs58PrivateKey(keypair1));
-
-    console.log(keypair2.publicKey.toBase58());
-    // console.log();
-    const privatekey = Account.getBs58PrivateKey(keypair2);
-
-    const keypair = Web3.Keypair.fromSecretKey(bs58.decode(privatekey));
-    console.log('publickey', keypair.publicKey.toString());
-    console.log('secretKey', keypair.secretKey.toString());
-    expect(keypair.publicKey.toString()).toEqual(keypair2.publicKey.toString());
-    expect(keypair.secretKey.toString()).toEqual(keypair2.secretKey.toString());
+  test('Tranfrom between Keypair and Privitekey', () => {
+    const privatekey = Account.getBs58PrivateKey(keypair1);
+    console.log('public key before', keypair1.publicKey.toBase58());
+    console.log('secret key before', keypair1.secretKey.toString());
+    console.log('privatekey before', privatekey);
+    const keypair = Account.getKeypairFromBs58PrivateKey(privatekey);
+    console.log('public key after', keypair.publicKey.toString());
+    console.log('secret key after', keypair.secretKey.toString());
+    expect(keypair.publicKey.toString()).toEqual(keypair1.publicKey.toString());
+    expect(keypair.secretKey.toString()).toEqual(keypair1.secretKey.toString());
   });
 });
